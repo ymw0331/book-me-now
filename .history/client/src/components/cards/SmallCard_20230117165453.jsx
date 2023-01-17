a5a09985bc80956b28c5e64b7b4389b93a550c36
Wayne Yong <ymw0331@gmail.com>
2023-01-18 00:28:10 +0800

    add users hotel bookings

---
+++ .history/client/src/components/cards/SmallCard_20230117165453.jsx
@@ -0,0 +1,96 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import { currencyFormatter } from '../../actions/stripe';
+import { diffDays } from '../../actions/hotel';
+import { useNavigate, Link } from 'react-router-dom';
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
+
+
+export default function SmallCard ( {
+  h,
+  handleHotelDelete = ( f ) => f,
+  owner = false,
+  showMoreViewButton = true
+} )
+{
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <div className='card mb-3'>
+        <div className='row nol-gutters'>
+          <div className='col-md-4'>
+            { h.image && h.image.contentType ? (
+              <img
+                className='card-image img img-fluid'
+                src={ `${ process.env.REACT_APP_API }/hotel/image/${ h._id }` }
+                alt='default hotel image'
+              />
+            ) : (
+              <img
+                className='card-image img img-fluid'
+                src='https://via.placeholder.com/900x500.png?text=MERN+Booking'
+                alt='default hotel image'
+              />
+            ) }
+
+          </div>
+
+          <div className='col-md-8'>
+            <div className='card-body'>
+              <h3 className='card-title'>
+                { h.title } <span className='float-right text-primary'>
+                  { currencyFormatter( {
+                    amount: h.price * 100,
+                    currency: "sgd"
+                  } ) }
+
+                </span>
+              </h3>
+              <p className='alert alert-info'>{ h.location }</p>
+              <p className='card-text'>{ `${ h.content.substring( 0, 200 ) }...` }</p>
+              <p classname='card-text'>
+                <span className='float-right text-primary'>
+                  for { diffDays( h.from, h.to ) }{ "" } { diffDays( h.from, h.to ) <= 1 ? 'day' : 'days' }
+                </span>
+              </p>
+
+              <p className='card-text'> { h.bed } bed </p>
+
+              <p className='card-text'>
+                Available from { new Date( h.from ).toLocaleDateString() }
+              </p>
+
+
+
+              <div className='d-flex justify-content-between h4'>
+                {
+                  showMoreViewButton &&
+                  <button className='btn btn-primary'
+                    onClick={ () => navigate( `/hotel/${ h._id }` ) }
+                  >Show More
+                  </button>
+                }
+
+
+                { owner && <>
+                  <Link to={ `/hotel/edit/${ h._id }` }>
+                    <EditOutlined className='text-warning' />
+                  </Link>
+
+                  <DeleteOutlined
+                    onClick={ () => handleHotelDelete( h._id ) }
+                    className="text-danger"
+                  />
+                </> }
+
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+    </> );
+
+}
---
+++ .history/server/routes/hotel_20230118001042.js
@@ -0,0 +1,24 @@
+import express from "express";
+import formidable from 'express-formidable';
+
+const router = express.Router();
+
+//middlewares
+import { requireSignin, hotelOwner } from "../middlewares/index";
+
+//controllers
+import { create, hotels, image, sellerHotels, remove, read, update ,userHotelBookings} from '../controllers/hotel';
+
+router.post( "/create-hotel", formidable(), requireSignin, create );
+router.get( "/hotels", hotels );
+router.get( "/hotel/image/:hotelId", image );
+router.get( "/seller-hotels", requireSignin, sellerHotels );
+router.delete( "/delete-hotel/:hotelId", requireSignin, hotelOwner, remove );
+router.get( "/hotel/:hotelId", read );
+router.put( "/update-hotel/:hotelId", requireSignin, hotelOwner, formidable(), update );
+// orders
+router.get("/user-hotel-bookings", requireSignin, userHotelBookings);
+router.get("/is-already-booked/:hotelId", requireSignin, isAlreadyBooked);
+
+// export default router;
+module.exports = router;
